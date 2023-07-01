3f900df9e9760b4a41394fc93d7ced2e78e8ba8b
Rajesh kumar <rk129479@gmail.com>
2023-07-02 01:01:38 +0530

    done

--- routes/userroute.js
+++ routes/userroute.js
@@ -5,21 +5,34 @@ const bcrypt=require("bcrypt")
 var jwt = require('jsonwebtoken');
 
 
-userroute.post("/add",async(req,res)=>{
-    let{email,name,password}=req.body
-    try {
-        bcrypt.hash(password, 2, async(err, hash)=> {
-            // Store hash in your password DB.
-            let data=new usermodel({name,email,password:hash})
-            await data.save()
-            console.log(data)
-            res.status(200).send({"msg":"Registration Successful"})
-        });
-    } catch (error) {
-        console.log(error)
-        res.status(400).send(error)
-    }
-
+userroute.get("/",(req,res)=>{
+    res.send("user route")
+})
+ 
+ 
+userroute.post("/add", async(req,res)=>{
+     try {
+         let {name,email,password}=req.body
+         let user=await usermodel.findOne({email})
+         console.log(user)
+ 
+         if(user){
+             return res.status(400).send({"msg":"already exist please login"})
+         }
+ 
+         let hashpasswod= bcrypt.hashSync(password,6)
+ 
+         let newuser= new usermodel({name,email,password:hashpasswod})
+         
+         let dbnewuser=await newuser.save()
+ 
+         
+         res.status(200).send({"msg":"User registered successfully."})
+  
+        } catch (error) {
+         res.status(400).send(error)
+         //console.log(error)
+        }
 })
 
 userroute.post("/login",async(req,res)=>{
@@ -31,7 +44,7 @@ userroute.post("/login",async(req,res)=>{
         if(data){
             bcrypt.compare(password, data.password, (err, result)=> {
                 if(result){
-                    res.status(200).send({"msg":`login sucessfull`,"name":data.name,"token":jwt.sign({"userid":data._id }, 'masai',{expiresIn:"1hr"})})
+                    res.status(200).send({"msg":`login sucessfull`,"name":data.name,"token":jwt.sign({"userid":data._id }, 'rajesh',{expiresIn:"1hr"})})
                 }else{
                     res.status(400).send({"msg":"user not found"})
                 }
